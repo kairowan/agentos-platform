eb093d3c51233bfcbfc195559bf47c6ea687c82a
haonan <gaohaonan@jimmycat.net>
2026-08-25 15:44:02 +0800

    Prepare v0.2.1

--- apps/AgentShell/build.gradle.kts
+++ apps/AgentShell/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.agentos.shell"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
